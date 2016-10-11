0da3a34ba2a2c3e1c23897ef23b04b29cfcb9556
Sanjit Bhardwaj <Sanjit Bhardwaj>
2016-10-11 22:23:30 +0530

    userdata error

--- wrapper/userDataHandler.php
+++ wrapper/userDataHandler.php
@@ -34,8 +34,9 @@ class userDataHandler {
         $city ='';
         if (isset($data['status'])) {
             $query = 'INSERT INTO guest (name,phoneNumber,city,numberOfPeople,dateOfArrival,dateOfDeparture,roomNumberAllotted,isCheckout,amountPaid,createdBy,createdTime)'
-                    . 'values("' . $data["name"] . '","' . $data["phoneNumber"] . '","' . $data["city"] . '","' . $data["numberOfPeople"] . '","' . $data["comingDate"] . '","' . $data["returnDate"] . '","' . $data['roomNumberAlloted'] . '",'0'"' . $data["amountPaid"] . '","' . $data["userId"] . '",now())';
+                    . 'values("' . $data["name"] . '","' . $data["phoneNumber"] . '","' . $data["city"] . '","' . $data["numberOfPeople"] . '","' . $data["comingDate"] . '","' . $data["returnDate"] . '","' . $data['roomNumberAlloted'] . '","0","' . $data["amountPaid"] . '","' . $data["userId"] . '",now())';
             $result = queryRunner::doInsert($query);
+            $sql1 = "SELECT city FROM guest WHERE roomNumberAllotted = '" . $data['roomNumberAlloted'] . "' AND isCheckout = '0'";
             $cityData = queryRunner::doSelect($sql1);
             if (isset($cityData)) {
                 for ($i = 0; $i < count($cityData); $i++) {
